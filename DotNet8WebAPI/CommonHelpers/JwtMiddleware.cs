768deeceb52aecf60294026908d651e213e6d926
Shubhang Sharma <shubhangsharma6@outlook.com>
2025-03-29 15:35:39 +0530

    Updating Program.cs file for enabling it to run in azure app service. Adding JWT Authentication initial files

---
+++ DotNet8WebAPI/CommonHelpers/JwtMiddleware.cs
@@ -0,0 +1,17 @@
+﻿using DotNet8WebAPI.Model;
+using Microsoft.Extensions.Options;
+
+namespace DotNet8WebAPI.CommonHelpers
+{
+    public class JwtMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly AppSettings _appSettings;
+
+        public JwtMiddleware(RequestDelegate next, IOptions<AppSettings> appSettings)
+        {
+                _next = next;
+                _appSettings = appSettings.Value;
+        }
+    }
+}
